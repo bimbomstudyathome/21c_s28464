c02e0a2f67d94f4cc1e0147e8a84a32a8d20f9df
bimbomstudyathome <s28464@pjwstk.edu.pl>
2024-03-15 16:30:26 +0100

    Tasks
    Task 1 and 2 are done

---
+++ main.py
@@ -0,0 +1,7 @@
+# square = [num**2 for num in range(1, 11)]
+# print(square)
+def toSquares(firstNum, secondNum):
+    square = [num**2 for num in range(1, 11)]
+    return square
+res = toSquares(1, 11)
+print(res)
