1dfc9203525181175220ee94f49261de91ddc1b9
bimbomstudyathome <s28464@pjwstk.edu.pl>
2024-03-15 16:34:01 +0100

    Task 3 is done

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # square = [num**2 for num in range(1, 11)]
 # print(square)
-def toSquares(firstNum, secondNum):
-    square = [num**2 for num in range(1, 11)]
-    return square
-res = toSquares(1, 11)
-print(res)
+# def toSquares(firstNum, secondNum):
+#     square = [num**2 for num in range(firstNum, secondNum)]
+#     return square
+# res = toSquares(1, 11)
+# print(res)
+
+# class Square:
+#     def toSquares(self, firstNum, secondNum):
+#         square = [num ** 2 for num in range(firstNum, secondNum)]
+#         return square
+# squareObj = Square()
+# res = squareObj.toSquares(1, 11)
+# print(res)
